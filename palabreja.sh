bd5e54474beba4503bd9872c5a8b25f5d7f3f071
Juanjo Escribano <jjescribano@tuenti.com>
2018-12-10 09:48:06 +0100

    Ahora puedes ejecutar esto desde donde más te apetezca

--- palabreja.sh
+++ palabreja.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-num_palabrejas=$(wc -l palabrejas | cut -d " " -f 1)
+script_directory=$(dirname $0)
+num_palabrejas=$(wc -l ${script_directory}/palabrejas | cut -d " " -f 1)
 random_num=$(( ( RANDOM % $num_palabrejas )  + 1 ))
-palabreja=$(sed "${random_num}q;d" palabrejas)
+palabreja=$(sed "${random_num}q;d" ${script_directory}/palabrejas)
 echo $palabreja
 echo "http://dle.rae.es/?w=$palabreja"
