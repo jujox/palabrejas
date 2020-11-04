a4911fef6eff71fad612f11425669c4c48cf66ce
Juanjo Escribano <jjescribano@tuenti.com>
2020-11-04 10:10:48 +0100

    Nuevas palabrejas

--- palabreja.sh
+++ palabreja.sh
@@ -5,4 +5,6 @@ num_palabrejas=$(wc -l ${script_directory}/palabrejas | cut -d " " -f 1)
 random_num=$(( ( RANDOM % $num_palabrejas )  + 1 ))
 palabreja=$(sed "${random_num}q;d" ${script_directory}/palabrejas)
 echo $palabreja
-echo "http://dle.rae.es/?w=$palabreja"
+echo -ne "Pulsa 'intro' para firefox http://dle.rae.es/?w=$palabreja"
+read
+firefox http://dle.rae.es/?w=$palabreja
